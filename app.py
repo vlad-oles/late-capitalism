e23d810977590df9df9ed0c469d2fe809d7c4713
me <xshadorx@gmail.com>
2018-09-05 15:48:38 -0700

    Quandl API test

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect
+import quandl
 
 
+quandl.ApiConfig.api_key = 'YarQnKztTjms_zWSmMZy'
+quandl.ApiConfig.api_version = '2015-04-09'
 app = Flask(__name__)
 
 
@@ -11,6 +14,7 @@ def index():
 
 @app.route('/about')
 def about():
+  quandl.Database('WIKI/PRICES').bulk_download_to_file('wikip.dat')
   return render_template('about.html')
 
 
